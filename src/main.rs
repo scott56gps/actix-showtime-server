545b5738e0a640d04145b97e9595712c8f18be10
Scott Nicholes <scott93gps@gmail.com>
2021-08-18 01:33:21 -0700

    Using database connection for GET requests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{HttpResponse, Responder, web, HttpServer, App};
+use actix_web::{HttpResponse, Responder, web, http, error::InternalError, HttpServer, App};
 
 mod db;
 mod model;
@@ -6,12 +6,27 @@ mod model;
 type StdErr = Box<dyn std::error::Error>;
 
 #[actix_web::get("/")]
-async fn hello(db: web::Data<db::DB>) -> impl Responder {
+async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hola from Actix!")
+    // web::Json(model::Movie { id: Some(5), title: "Rush Hour 2".to_string(), poster_url: "bueno".to_string() })
 }
 
-async fn echo(db: web::Data<db::DB>, req_body: String) -> impl Responder {
+#[actix_web::get("/watchlist")]
+async fn get_watchlist(db: web::Data<db::DB>) -> Result<web::Json<Vec<model::Movie>>, InternalError<StdErr>> {
+    db.movies()
+        .await
+        .map(web::Json)
+        .map_err(to_internal_error)
+}
+
+async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
+}
+
+// Private Functions
+fn to_internal_error(e: StdErr) -> InternalError<StdErr> {
+    println!("Received Error");
+    InternalError::new(e, http::StatusCode::INTERNAL_SERVER_ERROR)
 }
 
 #[actix_web::main]
@@ -25,6 +40,7 @@ async fn main() -> Result<(), StdErr> {
         App::new()
             .data(db.clone())
             .service(hello)
+            .service(get_watchlist)
             .route("/echo", web::post().to(echo))
     })
         .bind(("127.0.0.1", 8000))?
